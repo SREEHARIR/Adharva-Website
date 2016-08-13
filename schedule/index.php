0b967d322f34d846011c01544c6cdbab2a4833d8
R SREEHARI <sreehari.r96@gmail.com>
2016-08-13 13:01:43 +0530

    Schedule UI

---
+++ schedule/index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ADHARVA</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta charset="utf-8">
+    <meta name="author" content="SREEHARI">
+    <meta content="yes" name="apple-mobile-web-app-capable">
+    <meta content="ADHARVA" name="author">
+    <meta name="theme-color" content="#1d6a66">
+    <meta name="msapplication-navbutton-color" content="#1d6a66">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1d6a66">
+    <meta content="ADHARVA" name="description">
+    <meta content="ADHARVA" name="keywords">
+    <meta content="ADHARVA coming soon ">
+    <meta content="Techfest">
+    <meta content="website">
+    <meta content="index.php">
+    <meta content="ADHARVA">
+    <link href="../favicon.png" rel="shortcut icon" type="image/png">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="../images/favicon.png">
+    <link rel="apple-touch-icon" href="../images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../images/apple-touch-icon-114x114.png">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link href="../css/normalize.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style-responsive.css">
+    <link rel="stylesheet" href="../css/animate.min.css">
+    <link rel="stylesheet" href="../css/vertical-rhythm.min.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/magnific-popup.css">
+</head>
+
+<body class="appear-animate">
+    <?php include_once("../analyticstracking.php") ?>
+    <!-- Page Loader -->
+    <div class="page-loader">
+        <div class="loader">Loading...</div>
+    </div>
+    <!-- End Page Loader -->
+    <!-- Page Wrap -->
+    <div class="page" id="top">
+        <!-- Navigation panel -->
+        <nav class="main-nav js-stick">
+            <div class="full-wrapper relative clearfix">
+                <!-- Logo ( * your text or image into link tag *) -->
+                <div class="nav-logo-wrap local-scroll">
+                    <a href="../" class="logo">
+                        <img src="../images/logo.png" alt="" />
+                    </a>
+                </div>
+                <div class="mobile-nav">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <!-- Main Menu -->
+                <div class="inner-nav desktop-nav">
+                    <ul class="clearlist">
+                        <li>
+                            <a href="../">Home</a>
+                        </li>
+                        <li>
+                            <a href="../about">About</a>
+                        </li>
+                        <li>
+                            <a href="../events">Events</a>
+                        </li>
+                        <li>
+                            <a href="../schedule">Schedule</a>
+                        </li>
+                        <li>
+                            <a href="../register">Register</a>
+                        </li>
+                        <li>
+                            <a href="../team">Team</a>
+                        </li>
+                        <li>
+                            <a href="../contact">Contact</a> 
+                        </li>
+                    </ul>
+                </div>
+                <!-- End Main Menu -->
+            </div>
+        </nav>
+        <!-- Head Section -->
+        <section class="page-section bg-dark-alfa-70 parallax-3" data-background="../images/contact/contact-bg.jpg">
+            <div class="relative container align-left">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1 class="hs-line-11 font-alt mb-20 mb-xs-0">SCHEDULE</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Head Section -->
+        <section class="page-section">
+            <div class="container relative">
+                <!-- <h2 class="section-title font-alt mb-70 mb-sm-40">
+                        SCHEDULE
+                    </h2> -->
+                <div class="row mb-60 mb-xs-40">
+                    <div class="col-md-8 col-md-offset-2">
+                        <p>The schedule is subject to change without prior notice</p>
+                        <br>
+                        <table class="table table-bordered table-striped table-responsive ">
+                            <thead class="thead-inverse">
+                                <tr>
+                                    <th>
+                                        TIME SLOT
+                                    </th>
+                                    <th>
+                                        NAME OF EVENT
+                                    </th>
+                                    <th>
+                                        No. of rounds
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        10:00 AM - 10:30 AM
+                                    </td>
+                                    <td>
+                                        Inauguration
+                                    </td>
+                                    <td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        2:00 PM - 4:30 PM
+                                    </td>
+                                    <td>
+                                        Adharva Quiz
+                                    </td>
+                                    <td>
+                                        5
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        10:45 AM - 1:30 PM
+                                    </td>
+                                    <td>
+                                        Paper/Idea Presentation
+                                    </td>
+                                    <td>
+                                        1
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        10:00 AM - 4:00 PM
+                                    </td>
+                                    <td>
+                                        Exhibition
+                                    </td>
+                                    <td>
+                                        1
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        11:00  - 12:00<br>2:00 - 4:00PM
+                                    </td>
+                                    <td>
+                                        Junkyard Wars
+                                    </td>
+                                    <td>
+                                        2
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        1:30 PM - 4:00 PM
+                                    </td>
+                                    <td>
+                                        The Perfect Ambassador
+                                    </td>
+                                    <td>
+                                        4
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        11:00 AM - 4:00 PM</td>
+                                    <td>
+                                        RC Racing
+                                    </td>
+                                    <td>
+                                        1
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Foter -->
+        <footer class="page-section bg-gray-lighter footer pb-60">
+            <div class="container">
+                <!-- Footer Logo -->
+                <div class="local-scroll mb-30 wow fadeInUp" data-wow-duration="1.5s">
+                    <a href="#top"><img src="../images/logo-footer.png" width="200" height="36" alt="" /></a>
+                </div>
+                <!-- End Footer Logo -->
+                <!-- Social Links -->
+                <div class="footer-social-links mb-110 mb-xs-60">
+                    <a href="https://www.facebook.com/adharva17/" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/adharva_2k17/" title="Instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+                    <!-- 
+                    <a href="#" title="Behance" target="_blank"><i class="fa fa-behance"></i></a>
+                    <a href="#" title="LinkedIn+" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    <a href="#" title="Pinterest" target="_blank"><i class="fa fa-pinterest"></i></a> 
+                    -->
+                </div>
+                <!-- End Social Links -->
+                <!-- Footer Text -->
+                <!-- <div class="footer-text">
+                    <div class="footer-copy font-alt">
+                        <a href="http://themeforest.net/user/theme-guru/portfolio" target="_blank">&copy; Rhythm 2016</a>.
+                    </div>
+                    <div class="footer-made">
+                        Made with love for great people.
+                    </div>
+                </div> -->
+                <!-- End Footer Text -->
+            </div>
+            <!-- Top Link -->
+            <div class="local-scroll">
+                <a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+            </div>
+            <!-- End Top Link -->
+        </footer>
+        <!-- End Foter -->
+    </div>
+    <!-- End Page Wrap -->
+    <!-- JS -->
+    <script type="text/javascript" src="../js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/SmoothScroll.js"></script>
+    <script type="text/javascript" src="../js/jquery.scrollTo.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.localScroll.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.viewport.mini.js"></script>
+    <script type="text/javascript" src="../js/jquery.countTo.js"></script>
+    <script type="text/javascript" src="../js/jquery.appear.js"></script>
+    <script type="text/javascript" src="../js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="../js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="../js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="../js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="../js/isotope.pkgd.min.js"></script>
+    <script type="text/javascript" src="../js/imagesloaded.pkgd.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="../js/wow.min.js"></script>
+    <script type="text/javascript" src="../js/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.simple-text-rotator.min.js"></script>
+    <script type="text/javascript" src="../js/all.js"></script>
+    <!--[if lt IE 10]><script type="text/javascript" src="../js/placeholder.js"></script><![endif]-->
+</body>
+
+</html>
