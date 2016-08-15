25881b7710b0dea7c80627c25e325e340d18cc70
R SREEHARI <sreehari.r96@gmail.com>
2016-08-15 13:01:43 +0530

    Events UI

---
+++ events/Flash-fiction/index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ADHARVA</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta charset="utf-8">
+    <meta name="author" content="SREEHARI">
+    <meta content="yes" name="apple-mobile-web-app-capable">
+    <meta content="ADHARVA" name="author">
+    <meta name="theme-color" content="#1d6a66">
+    <meta name="msapplication-navbutton-color" content="#1d6a66">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1d6a66">
+    <meta content="ADHARVA" name="description">
+    <meta content="ADHARVA" name="keywords">
+    <meta content="ADHARVA coming soon ">
+    <meta content="Techfest">
+    <meta content="website">
+    <meta content="index.php">
+    <meta content="ADHARVA">
+    <link href="../../favicon.png" rel="shortcut icon" type="image/png">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="../../images/favicon.png">
+    <link rel="apple-touch-icon" href="../../images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../../images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../../images/apple-touch-icon-114x114.png">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/style-responsive.css">
+    <link rel="stylesheet" href="../../css/animate.min.css">
+    <link rel="stylesheet" href="../../css/vertical-rhythm.min.css">
+    <link rel="stylesheet" href="../../css/owl.carousel.css">
+    <link rel="stylesheet" href="../../css/magnific-popup.css">
+</head>
+
+<body class="appear-animate">
+    <?php include_once("../../analyticstracking.php") ?>
+    <!-- Page Loader -->
+    <div class="page-loader">
+        <div class="loader">Loading...</div>
+    </div>
+    <!-- End Page Loader -->
+    <!-- Page Wrap -->
+    <div class="page" id="top">
+        <!-- Navigation panel -->
+        <nav class="main-nav js-stick">
+            <div class="full-wrapper relative clearfix">
+                <!-- Logo ( * your text or image into link tag *) -->
+                <div class="nav-logo-wrap local-scroll">
+                    <a href="../../" class="logo">
+                        <img src="../../images/logo.png" alt="" />
+                    </a>
+                </div>
+                <div class="mobile-nav">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <!-- Main Menu -->
+                <div class="inner-nav desktop-nav">
+                    <ul class="clearlist">
+                        <li>
+                            <a href="../../">Home</a>
+                        </li>
+                        <li>
+                            <a href="../../about">About</a>
+                        </li>
+                        <li>
+                            <a href="../../events">Events</a>
+                        </li>
+                        <li>
+                            <a href="../../schedule">Schedule</a>
+                        </li>
+                        <li>
+                            <a href="../../register">Register</a>
+                        </li>
+                        <li>
+                            <a href="../../team">Team</a>
+                        </li>
+                        <li>
+                            <a href="../../contact">Contact</a> 
+                        </li>
+                    </ul>
+                </div>
+                <!-- End Main Menu -->
+            </div>
+        </nav>
+        <!-- End Navigation panel -->
+        <!-- Section -->
+        <section class="page-section">
+            <div class="container relative">
+                <div class="row">
+                    <!-- Content -->
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <!-- Post -->
+                        <div class="blog-item mb-80 mb-xs-40">
+                            <!-- Text -->
+                            <div class="blog-item-body">
+                                <h2 class="mt-0 font-alt">Flash Fiction</h2>
+                                <div class="lead">
+                                    <p>
+                                        Use your words wisely!
+                                    </p>
+                                </div>
+                                <!-- End Text -->
+                                <!-- Media Gallery -->
+                                <div class="blog-media mt-40 mb-40 mb-xs-30">
+                                    <ul class="clearlist content-slider">
+                                        <li>
+                                            <img src="../../images/events/flashfiction-big.jpg" alt="" />
+                                        </li>
+                                    </ul>
+                                </div>
+                                <p>
+                                    <br>
+                                    <br>Love writing? If you have an amazing imagination and the instant ability to improvise, then here’s the challenge for the writer in you. Use your ability with words to breathe a story into life by connecting the beginning and the end. How efficient are you at weaving a story with minimum words? Let’s find out!
+                                </p>
+                                <p>
+                                    PRIZE MONEY
+                                    <br> Top 2 Fiction : Rs. 1000/-
+                                </p>
+                                <p>
+                                    Registration
+                                    <br> Free Entry
+                                </p>
+                                <p>
+                                    Rules
+                                </p>
+                                <ul>
+                                    <li>Your Entry must be a story on “Trivandrum” within 180 characters (including spaces).</li>
+                                    <li>Maximum of 3 unique entries allowed per writer.</li>
+                                    <li>Obscenity in writings should be avoided.</li>
+                                    <li>Must not be a part of any poem.</li>
+                                    <li>Submissions made should be the original work of the writer and not published on any other platform.</li>
+                                    <li>All the entries will be screened and published in the Flash Fiction facebook page.The best two entries will be chosen and entitled to a cash prize of Rs 1000 each.</li>
+                                    <li>Open to all engineering colleges</li>
+                                </ul>
+<!--                                 <div class="clearfix">
+                                    <div class="align-center pt-10">
+                                        <a href="./entry">
+                                            <button class="submit_btn btn btn-mod btn-medium btn-round">Submit FLASH FICTION</button>
+                                        </a>
+                                    </div>
+                                </div>-->
+                                <h4> <b>WE are no longer accepting any Fiction. Thank You guys for your overwhelming response. The winners will be announced shortly.</b></h4>
+
+                                <p>
+                                    Contact:
+                                    <br>Sruthi Thomas - +91 8943362432
+                                    <br>Riyanka Johnson - +91 9544229520
+                                </p>
+                            </div>
+                            <!-- End Text -->
+                        </div>
+                        <!-- End Post -->
+                    </div>
+                    <!-- End Content -->
+                </div>
+            </div>
+        </section>
+        <!-- End Section -->
+        <!-- Foter -->
+        <footer class="page-section bg-gray-lighter footer pb-60">
+            <div class="container">
+                <!-- Footer Logo -->
+                <div class="local-scroll mb-30 wow fadeInUp" data-wow-duration="1.5s">
+                    <a href="#top"><img src="../../images/logo-footer.png" width="200" height="36" alt="" /></a>
+                </div>
+                <!-- End Footer Logo -->
+                <!-- Social Links -->
+                <div class="footer-social-links mb-110 mb-xs-60">
+                    <a href="https://www.facebook.com/adharva17/" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/adharva_2k17/" title="Instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+                    <!-- 
+                    <a href="#" title="Behance" target="_blank"><i class="fa fa-behance"></i></a>
+                    <a href="#" title="LinkedIn+" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    <a href="#" title="Pinterest" target="_blank"><i class="fa fa-pinterest"></i></a> 
+                    -->
+                </div>
+                <!-- End Social Links -->
+                <!-- Footer Text -->
+                <!-- <div class="footer-text">
+                    <div class="footer-copy font-alt">
+                        <a href="http://themeforest.net/user/theme-guru/portfolio" target="_blank">&copy; Rhythm 2016</a>.
+                    </div>
+                    <div class="footer-made">
+                        Made with love for great people.
+                    </div>
+                </div> -->
+                <!-- End Footer Text -->
+            </div>
+            <!-- Top Link -->
+            <div class="local-scroll">
+                <a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+            </div>
+            <!-- End Top Link -->
+        </footer>
+        <!-- End Foter -->
+    </div>
+    <!-- End Page Wrap -->
+    <!-- JS -->
+    <script type="text/javascript" src="../../js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../../js/SmoothScroll.js"></script>
+    <script type="text/javascript" src="../../js/jquery.scrollTo.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.localScroll.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.viewport.mini.js"></script>
+    <script type="text/javascript" src="../../js/jquery.countTo.js"></script>
+    <script type="text/javascript" src="../../js/jquery.appear.js"></script>
+    <script type="text/javascript" src="../../js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="../../js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="../../js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="../../js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="../../js/isotope.pkgd.min.js"></script>
+    <script type="text/javascript" src="../../js/imagesloaded.pkgd.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="../../js/gmap3.min.js"></script>
+    <script type="text/javascript" src="../../js/wow.min.js"></script>
+    <script type="text/javascript" src="../../js/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.simple-text-rotator.min.js"></script>
+    <script type="text/javascript" src="../../js/all.js"></script>
+    <script type="text/javascript" src="../../js/contact-form.js"></script>
+    <script type="text/javascript" src="../../js/jquery.ajaxchimp.min.js"></script>
+    <!--[if lt IE 10]><script type="text/javascript" src="../../js/placeholder.js"></script><![endif]-->
+</body>
+
+</html>
---
+++ events/Quiz/index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ADHARVA</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta charset="utf-8">
+    <meta name="author" content="SREEHARI">
+    <meta content="yes" name="apple-mobile-web-app-capable">
+    <meta content="ADHARVA" name="author">
+    <meta name="theme-color" content="#1d6a66">
+    <meta name="msapplication-navbutton-color" content="#1d6a66">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1d6a66">
+    <meta content="ADHARVA" name="description">
+    <meta content="ADHARVA" name="keywords">
+    <meta content="ADHARVA coming soon ">
+    <meta content="Techfest">
+    <meta content="website">
+    <meta content="index.php">
+    <meta content="ADHARVA">
+    <link href="../../favicon.png" rel="shortcut icon" type="image/png">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="../../images/favicon.png">
+    <link rel="apple-touch-icon" href="../../images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../../images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../../images/apple-touch-icon-114x114.png">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/style-responsive.css">
+    <link rel="stylesheet" href="../../css/animate.min.css">
+    <link rel="stylesheet" href="../../css/vertical-rhythm.min.css">
+    <link rel="stylesheet" href="../../css/owl.carousel.css">
+    <link rel="stylesheet" href="../../css/magnific-popup.css">
+</head>
+
+<body class="appear-animate">
+    <?php include_once("../../analyticstracking.php") ?>
+    <!-- Page Loader -->
+    <div class="page-loader">
+        <div class="loader">Loading...</div>
+    </div>
+    <!-- End Page Loader -->
+    <!-- Page Wrap -->
+    <div class="page" id="top">
+        <!-- Navigation panel -->
+        <nav class="main-nav js-stick">
+            <div class="full-wrapper relative clearfix">
+                <!-- Logo ( * your text or image into link tag *) -->
+                <div class="nav-logo-wrap local-scroll">
+                    <a href="../../" class="logo">
+                        <img src="../../images/logo.png" alt="" />
+                    </a>
+                </div>
+                <div class="mobile-nav">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <!-- Main Menu -->
+                <div class="inner-nav desktop-nav">
+                    <ul class="clearlist">
+                        <li>
+                            <a href="../../">Home</a>
+                        </li>
+                        <li>
+                            <a href="../../about">About</a>
+                        </li>
+                        <li>
+                            <a href="../../events">Events</a>
+                        </li>
+                        <li>
+                            <a href="../../schedule">Schedule</a>
+                        </li>
+                        <li>
+                            <a href="../../register">Register</a>
+                        </li>
+                        <li>
+                            <a href="../../team">Team</a>
+                        </li>
+                        <li>
+                            <a href="../../contact">Contact</a> 
+                        </li>
+                    </ul>
+                </div>
+                <!-- End Main Menu -->
+            </div>
+        </nav>
+        <!-- End Navigation panel -->
+        <!-- Section -->
+        <section class="page-section">
+            <div class="container relative">
+                <div class="row">
+                    <!-- Content -->
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <!-- Post -->
+                        <div class="blog-item mb-80 mb-xs-40">
+                            <!-- Text -->
+                            <div class="blog-item-body">
+                                <h2 class="mt-0 font-alt">ADHARVA Quiz</h2>
+                                <div class="lead">
+                                    <p>
+                                        Welcome to the marathon of minds!
+                                    </p>
+                                </div>
+                                <!-- End Text -->
+                                <!-- Media Gallery -->
+                                <div class="blog-media mt-40 mb-40 mb-xs-30">
+                                    <ul class="clearlist content-slider">
+                                        <li>
+                                            <img src="../../images/events/quiz-big.jpg" alt="" />
+                                        </li>
+                                    </ul>
+                                </div>
+                                <p>
+                                    <br>
+                                    <br>An open challenge to all those bright minds out there: here’s a chance to test your wits and skills in the various disciplines of technology. This arena will challenge the plethora of your knowledge – are you ready to put your skills to the test? Then come join us in our Tech Quiz! </p>
+                                <p>
+                                    PRIZE MONEY
+                                    <br> 1st : Rs. 5000/-
+                                    <br> 2nd : Rs. 3000/-
+                                    <br> 3rd : Rs. 2000/-
+                                </p>
+                                <p>
+                                    Registration
+                                    <br> Rs. 100/-
+                                </p>
+                                <p>
+                                    Rules
+                                </p>
+                                
+                                <ul>
+                                    <li>Consists of 5 to 6 responds.</li>
+                                    <li>Rounds will be announced on the spot.</li>
+                                    <li>All decisions taken by the quiz master will be final</li>
+                                </ul>
+                                <br>
+
+                                <!-- <ul>
+                                    <li>Subject/Field Round
+                                        <br>
+                                        <br>The participants will be asked a set of 5 questions depending on the subject/field they choose, from the set given by the quizmaster.
+                                        <br>No clues will be provided
+                                        <br>Passing of questions will not take place
+                                        <br><br>
+                                    </li>
+                                    <li>Know Your India
+                                        <br>
+                                        <br>Two questions per team. Questions deal with the history, culture, geography, economy etc . of our country.
+                                        <br>Clues will be provided
+                                        <br>Passing allowed
+                                        <br>Negative marks for each wrong answer
+                                        <br><br>
+                                    </li>
+                                    <li>Current Affairs in Technology
+                                        <br>
+                                        <br>Questions related with events of current importance.
+                                        <br>3 questions per team.
+                                        <br>Clues will not be provided.
+                                        <br>Passing allowed
+                                        <br>Negative marking for wrong answers
+                                        <br><br>
+                                    </li>
+                                    <li>Who is Who
+                                        <br>
+                                        <br>A blurred image of a famous personality with a small portion of the image intact; some information regarding the person will be given along wth the question.
+                                        <br>Further clues with reduction in points
+                                        <br>No passing of questions.
+                                        <br><br>
+                                    </li>
+                                    <li>Rapid Fire
+                                        <br>
+                                        <br>Each question will face 10 questions within 90 seconds.
+                                        <br>Negative Marks
+                                        <br>No clues
+                                    <br><br>
+                                    </li>
+                                </ul> -->
+                                <p>
+                                    Contact: 
+                                    <br>Alza Grace - +91 8281918138
+                                    <br>Fathimathu Zahira - +91 9995651160
+                                </p>
+                            </div>
+                            <!-- End Text -->
+                        </div>
+                        <!-- End Post -->
+                    </div>
+                    <!-- End Content -->
+                </div>
+            </div>
+        </section>
+        <!-- End Section -->
+        <!-- Foter -->
+        <footer class="page-section bg-gray-lighter footer pb-60">
+            <div class="container">
+                <!-- Footer Logo -->
+                <div class="local-scroll mb-30 wow fadeInUp" data-wow-duration="1.5s">
+                    <a href="#top"><img src="../../images/logo-footer.png" width="200" height="36" alt="" /></a>
+                </div>
+                <!-- End Footer Logo -->
+                <!-- Social Links -->
+                <div class="footer-social-links mb-110 mb-xs-60">
+                    <a href="https://www.facebook.com/adharva17/" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/adharva_2k17/" title="Instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+                    <!-- 
+                    <a href="#" title="Behance" target="_blank"><i class="fa fa-behance"></i></a>
+                    <a href="#" title="LinkedIn+" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    <a href="#" title="Pinterest" target="_blank"><i class="fa fa-pinterest"></i></a> 
+                    -->
+                </div>
+                <!-- End Social Links -->
+                <!-- Footer Text -->
+                <!-- <div class="footer-text">
+                    <div class="footer-copy font-alt">
+                        <a href="http://themeforest.net/user/theme-guru/portfolio" target="_blank">&copy; Rhythm 2016</a>.
+                    </div>
+                    <div class="footer-made">
+                        Made with love for great people.
+                    </div>
+                </div> -->
+                <!-- End Footer Text -->
+            </div>
+            <!-- Top Link -->
+            <div class="local-scroll">
+                <a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+            </div>
+            <!-- End Top Link -->
+        </footer>
+        <!-- End Foter -->
+    </div>
+    <!-- End Page Wrap -->
+    <!-- JS -->
+    <script type="text/javascript" src="../../js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../../js/SmoothScroll.js"></script>
+    <script type="text/javascript" src="../../js/jquery.scrollTo.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.localScroll.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.viewport.mini.js"></script>
+    <script type="text/javascript" src="../../js/jquery.countTo.js"></script>
+    <script type="text/javascript" src="../../js/jquery.appear.js"></script>
+    <script type="text/javascript" src="../../js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="../../js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="../../js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="../../js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="../../js/isotope.pkgd.min.js"></script>
+    <script type="text/javascript" src="../../js/imagesloaded.pkgd.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="../../js/gmap3.min.js"></script>
+    <script type="text/javascript" src="../../js/wow.min.js"></script>
+    <script type="text/javascript" src="../../js/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.simple-text-rotator.min.js"></script>
+    <script type="text/javascript" src="../../js/all.js"></script>
+    <script type="text/javascript" src="../../js/contact-form.js"></script>
+    <script type="text/javascript" src="../../js/jquery.ajaxchimp.min.js"></script>
+    <!--[if lt IE 10]><script type="text/javascript" src="../../js/placeholder.js"></script><![endif]-->
+</body>
+
+</html>
